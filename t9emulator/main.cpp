75591f5bc9f4702820f55c95a2b53664c3d38302
Clément TARIEL <clement.tariel@gmail.com>
2021-11-30 11:57:39 +0100

    affichage image avec Qt

--- t9emulator/main.cpp
+++ t9emulator/main.cpp
@@ -1,4 +1,8 @@
 #include <QApplication>
+#include <QWidget>
+#include <QLabel>
+#include <QPixmap>
+#include <QGridLayout>
 #include <QPushButton>
 
 
@@ -6,8 +10,28 @@ int main(int argc, char *argv[])
 {
     QApplication app(argc, argv);
 
-    QPushButton bouton("l'emulateur de t9 n'est pas encore disponible voici un bouton a la place");
+    QWidget *widget = new QWidget;
+    QLabel  *label  = new QLabel;
+    QPixmap *pixmap_img = new QPixmap(QCoreApplication::applicationDirPath()+"/1110i.jpg");
+    // mon_logo se trouve dans le repertoire qui contient mon exe
+    //int w = label->width();
+    //int h = label->height();
+    //taille prise au pif
+    int w = 650;
+    int h = 650;
 
+    // set a scaled pixmap to a w x h window keeping its aspect ratio
+    label->setPixmap(pixmap_img->scaled(w,h,Qt::KeepAspectRatio));
+    //label->setPixmap(*pixmap_img);
+
+    QGridLayout *gridLayout = new QGridLayout(widget);
+    gridLayout->addWidget(label, 0, 0);
+    widget->setLayout(gridLayout);
+
+    widget->show();
+
+
+    QPushButton bouton("l'emulateur de t9 n'est pas encore disponible voici un bouton a la place");
     bouton.show();
 
     return app.exec();

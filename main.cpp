f1045765af96ae8ecbf9a807910164354c7e166a
Clément TARIEL <clement.tariel@gmail.com>
2022-01-04 19:22:23 +0100

    test affichage des propositions de mots

--- main.cpp
+++ main.cpp
@@ -53,7 +53,8 @@ int main()
     //nokia_screen.setSize(sf::Vector2f(phone_w*0.68,phone_h*0.235));
     nokia_screen.setSize(sf::Vector2f(phone_w*0.64,phone_h*0.19));
     // set color
-    nokia_screen.setFillColor(sf::Color(162, 160, 39));
+    sf::Color screen_color(162, 160, 39);
+    nokia_screen.setFillColor(screen_color);
     // set position
     //nokia_screen.setPosition(phone_w*0.158,phone_h*0.22);
     nokia_screen.setPosition(phone_w*0.1775,phone_h*0.245);
@@ -61,6 +62,8 @@ int main()
 
 
     std::vector<char> full_text;
+
+    std::vector<char> current_word;
 
     std::vector<sf::Text> linesTexts;
 
@@ -94,6 +97,39 @@ int main()
     // set position
     blinkText.setPosition(phone_w*0.19,phone_h*0.24);
 
+
+    sf::Text best_guess_text;
+    // select the font
+    best_guess_text.setFont(nokiafont);
+    // define text
+    best_guess_text.setString("Proof");
+    // set the character size
+    best_guess_text.setCharacterSize(charSize); // in pixels, not points!
+    // set the color
+    best_guess_text.setFillColor(screen_color);
+    // set the text style
+    //text.setStyle(sf::String::Bold | sf::String::Italic | sf::String::Underlined);
+    best_guess_text.setStyle(sf::Text::Regular);
+    // set position
+    best_guess_text.setPosition(phone_w*0.19,phone_h*0.24);
+
+    sf::Text guess_list_text;
+    // select the font
+    guess_list_text.setFont(nokiafont);
+    // define text
+    guess_list_text.setString("of\nconcept");
+    // set the character size
+    guess_list_text.setCharacterSize(charSize); // in pixels, not points!
+    // set the color
+    guess_list_text.setFillColor(sf::Color::Black);
+    // set the text style
+    //text.setStyle(sf::String::Bold | sf::String::Italic | sf::String::Underlined);
+    guess_list_text.setStyle(sf::Text::Regular);
+    // set position
+    guess_list_text.setPosition(phone_w*0.19,phone_h*0.24);
+
+    sf::RectangleShape best_gess_background(sf::Vector2f(0, 0));;
+    best_gess_background.setFillColor(sf::Color::Black);
     
 
 
@@ -125,7 +161,11 @@ int main()
     int num_current_line = 0;
     int num_max_line = 0;
     int num_current_line_on_screen = 0;
+    int line_offset;
+    int nb_guess_printed = 0;
+    int nb_lines_printed = 0;
 
+    bool key_is_new_char;
 
 
 
@@ -142,12 +182,71 @@ int main()
             else if ( event.type == sf::Event::TextEntered ){
                 if ( event.text.unicode < 0x80 ) {// it's printable
                     keyString = (char)event.text.unicode;
-                    
+                    key_is_new_char = false;
                     if ((keyString-'0' >= 0 && keyString-'0' <= 9)||keyString=='#'||keyString=='*'){
+                        key_is_new_char = true;
+                        if (keyString == '0'){
+                            nb_guess_printed = 0; // fin d'un mot
+                            if (current_word.size() == 1){
+                                switch(current_word[0]){
+                                    case ' ':
+                                        keyString = '.';
+                                        break;
+                                    case '.':
+                                        keyString = '.';
+                                        break;
+                                    default:
+                                        keyString = ' ';
+                                        break;
+                                }
+                            }else{
+                                keyString = ' ';
+                            }
+                            current_word.clear();
+                        }else{
+                            if (current_word.size() == 1){
+                                switch(current_word[0]){
+                                    case ' ':
+                                        current_word.clear();
+                                        break;
+                                    case '.':
+                                        current_word.clear();
+                                        break;
+                                    default:
+                                        break;
+                                }
+                            }
+                            //prediction etc
+                            //===============================
+                            //
+                            //  proposer une liste de
+                            //  suggestion plausibles
+                            //
+                            //===============================
+
+                            //appeller ici la fonction qui prend word en argument
+                            //et qui renvoie une liste de suggestions
+                            nb_guess_printed = 3;//valeur arbritraire en attendant
+                            
+                        }
+                        current_word.push_back(keyString);
                         while (!(num_current_line*NB_CHAR_PER_LINE+current_line_pos<full_text.size())){
                             full_text.push_back(' ');
                         }
-                        full_text[num_current_line*NB_CHAR_PER_LINE+current_line_pos] = keyString;
+                        if (num_current_line*NB_CHAR_PER_LINE+current_line_pos > 0){
+                            switch(current_word[0]){
+                                case '.':
+                                    full_text[num_current_line*NB_CHAR_PER_LINE+current_line_pos-1] = keyString;
+                                    full_text[num_current_line*NB_CHAR_PER_LINE+current_line_pos] = ' ';
+                                    break;
+                                default:
+                                    full_text[num_current_line*NB_CHAR_PER_LINE+current_line_pos] = keyString;
+                                    break;
+                            }
+                            
+                        }else{
+                            full_text[num_current_line*NB_CHAR_PER_LINE+current_line_pos] = keyString;
+                        }
                         char buf [NB_CHAR_PER_LINE];
                         for (int l=0;l<NB_CHAR_PER_LINE;l++){
                             if (num_current_line*NB_CHAR_PER_LINE+l<full_text.size()){
@@ -205,7 +304,7 @@ int main()
                         }
                         
                     }
-                    if ((keyString-'0' >= 0 && keyString-'0' <= 9)||keyString=='#'||keyString=='*'/*|| keyString == (char)13*/){
+                    if (key_is_new_char){
                         current_line_pos ++;
                         if (!(current_line_pos < NB_CHAR_PER_LINE)){
                             num_current_line ++;
@@ -214,7 +313,9 @@ int main()
                                 num_current_line_on_screen = NB_LINES_ON_SCREEN - 1;
                             }
                             current_line_pos = 0;
-                        }/*
+                        }
+                                               
+                        /*
                         if (keyString == (char)13){
                             num_current_line ++;
                             num_current_line_on_screen ++;
@@ -224,6 +325,7 @@ int main()
                             current_line_pos = 0;
                         }*/
                     }
+                    
                     while (num_max_line < num_current_line /*|| (current_line_pos == NB_CHAR_PER_LINE-1 && num_max_line == num_current_line)/**/){
                         num_max_line ++;
                         //
@@ -258,6 +360,9 @@ int main()
             }
             if (sf::Keyboard::isKeyPressed(sf::Keyboard::Left)){
                 // left key is pressed
+
+                nb_guess_printed = 0; //coder la cmpletion autoamtique
+
                 if (current_line_pos > 0){
                     current_line_pos --;
                 }else if(current_line_pos <= 0 && num_current_line>0){
@@ -271,6 +376,9 @@ int main()
             }
             if (sf::Keyboard::isKeyPressed(sf::Keyboard::Right)){
                 // right key is pressed
+                
+                nb_guess_printed = 0; //coder la cmpletion autoamtique
+
                 if (current_line_pos < NB_CHAR_PER_LINE - 1){
                     current_line_pos ++;
                 }else if(current_line_pos == NB_CHAR_PER_LINE - 1 && num_current_line < num_max_line){
@@ -284,6 +392,9 @@ int main()
             }
             if (sf::Keyboard::isKeyPressed(sf::Keyboard::Up)){
                 // up key is pressed
+
+                nb_guess_printed = 0; //coder la cmpletion autoamtique
+
                 if (num_current_line > 0){
                     num_current_line --;
                     num_current_line_on_screen --;
@@ -294,6 +405,9 @@ int main()
             }
             if (sf::Keyboard::isKeyPressed(sf::Keyboard::Down)){
                 // down key is pressed
+
+                nb_guess_printed = 0; //coder la cmpletion autoamtique
+
                 if (num_current_line < num_max_line){
                     num_current_line ++;
                     num_current_line_on_screen ++;
@@ -316,9 +430,46 @@ int main()
         window.draw(phone_sprite);
 
         window.draw(nokia_screen);
+
         
+        if (num_max_line < NB_LINES_ON_SCREEN-1){
+            nb_lines_printed = num_max_line+1;
+        }else{
+            nb_lines_printed = NB_LINES_ON_SCREEN;
+        }
+
+        if (nb_guess_printed > 0){
+            if (num_current_line_on_screen + nb_guess_printed < NB_LINES_ON_SCREEN){
+                line_offset = num_current_line_on_screen;
+            }else{
+                line_offset = NB_LINES_ON_SCREEN-1-nb_guess_printed ;
+            }
+            while (nb_lines_printed + nb_guess_printed > NB_LINES_ON_SCREEN){
+                nb_lines_printed --;
+            }
+
+            best_gess_background.setPosition(phone_w*0.19-1,phone_h*0.24 + nb_lines_printed*charSize);
+            best_gess_background.setSize(sf::Vector2f(best_guess_text.getLocalBounds().width+2,charSize+2));
+            window.draw(best_gess_background);
+            
+            best_guess_text.setPosition(phone_w*0.19,phone_h*0.24 + nb_lines_printed*charSize);
+            window.draw(best_guess_text);
+            
+            guess_list_text.setPosition(phone_w*0.19,phone_h*0.24 + (nb_lines_printed+1)*charSize);
+            window.draw(guess_list_text);
+        }else{
+            line_offset = num_current_line_on_screen;
+        }
+
+        for (int k=0;k<nb_lines_printed;k++){
+            linesTexts[k+num_current_line-line_offset].setPosition(phone_w*0.19,phone_h*0.24 + k*charSize);
+            
+            window.draw(linesTexts[k+num_current_line-line_offset]);
+            
+        }
+
         //window.draw(text);
-        if (!(num_max_line < NB_LINES_ON_SCREEN-1)){
+        /*if (!(num_max_line < NB_LINES_ON_SCREEN-1)){
             /*int local_num_lines_to_print = NB_LINES_ON_SCREEN;
             if (current_line_pos >= NB_CHAR_PER_LINE-1 && num_current_line_on_screen == NB_LINES_ON_SCREEN-1){
                 local_num_lines_to_print --;
@@ -327,10 +478,13 @@ int main()
                 
                 linesTexts[k+num_current_line-local_num_lines_to_print+NB_LINES_ON_SCREEN- num_current_line_on_screen].setPosition(phone_w*0.19,phone_h*0.24 + k*charSize);
                 window.draw(linesTexts[k+num_current_line-local_num_lines_to_print+NB_LINES_ON_SCREEN-num_current_line_on_screen]);
-            }*/
+            }*//*
+
             for (int k=0;k<NB_LINES_ON_SCREEN;k++){
-                linesTexts[k+num_current_line-num_current_line_on_screen].setPosition(phone_w*0.19,phone_h*0.24 + k*charSize);
-                window.draw(linesTexts[k+num_current_line-num_current_line_on_screen]);
+                linesTexts[k+num_current_line-line_offset].setPosition(phone_w*0.19,phone_h*0.24 + k*charSize);
+                
+                window.draw(linesTexts[k+num_current_line-line_offset]);
+                
             }
         }else{
             for (int k=0;k<num_max_line+1;k++){
@@ -338,12 +492,13 @@ int main()
                 linesTexts[k].setPosition(phone_w*0.19,phone_h*0.24 + k*charSize);
                 
                 window.draw(linesTexts[k]);
+                
             }
-        }
+        }*/
 
         if ((int)floor(2*elapsed.asSeconds())%2 == 0){
             int cursor_x_pos = current_line_pos;
-            int cursor_y_pos = num_current_line_on_screen;
+            int cursor_y_pos = line_offset;
             if (cursor_x_pos >= NB_CHAR_PER_LINE){
                 cursor_x_pos = 0;
                 cursor_y_pos ++;
